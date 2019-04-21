b09504980585287de873a51deac71844defcee86
Rodger Byrd <rodger.byrd@gmail.com>
2019-04-21 15:02:54 -0600

    adding scripting and output

---
+++ script.sh
@@ -0,0 +1,4 @@
+grep -E "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b"  user-ct-test-collection-10.txt >> ~/school/CS5930/project/source/email.txt
+rodger@rodger-K55A:~/school/CS5930/project/AOL_search_data_leak_2006/AOL-user-ct-collection$ 
+
+
